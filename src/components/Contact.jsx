e193e6b49237743287bafb9e98da8c8c1889c491
Girish_N <ngirish1729@gmail.com>
2023-07-26 11:54:34 +0530

    added contact form functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,45 +1,51 @@
 export default function Contact() {
   return (
-    <div className="contact--container">
-      <p className="contact--head">CONTACT ME</p>
-      <input
-        type="text"
-        className="contact--input"
-        name="contact--name"
-        placeholder="Name"
-        autoFocus
-        required
-        autoComplete="false"
-      ></input>
-      <input
-        type="email"
-        className="contact--input"
-        name="contact--mail"
-        placeholder="e-mail"
-        required
-        autoComplete="false"
-        autoCapitalize="false"
-      ></input>
-      <input
-        type="text"
-        className="contact--input"
-        name="contact--subject"
-        placeholder="Message Subject"
-        required
-        autoComplete="false"
-      ></input>
-      <textarea
-        rows={5}
-        type="text"
-        className="contact--input"
-        name="contact--message"
-        placeholder="Message"
-        required
-        autoComplete="false"
-      ></textarea>
-      <div className="contact--send">
-        <button type="submit">SEND</button>
+    <form
+      action="https://contact-api-girish-n7.vercel.app/"
+      id="contact--form"
+      method="POST"
+    >
+      <div className="contact--container">
+        <p className="contact--head">CONTACT ME</p>
+        <input
+          type="text"
+          className="contact--input"
+          name="contactName"
+          placeholder="Name"
+          autoFocus
+          required
+          autoComplete="false"
+        ></input>
+        <input
+          type="email"
+          className="contact--input"
+          name="contactMail"
+          placeholder="e-mail"
+          required
+          autoComplete="false"
+          autoCapitalize="false"
+        ></input>
+        <input
+          type="text"
+          className="contact--input"
+          name="contactSubject"
+          placeholder="Message Subject"
+          required
+          autoComplete="false"
+        ></input>
+        <textarea
+          rows={5}
+          type="text"
+          className="contact--input"
+          name="contactMessage"
+          placeholder="Message"
+          required
+          autoComplete="false"
+        ></textarea>
+        <div className="contact--send">
+          <button type="submit">SEND</button>
+        </div>
       </div>
-    </div>
+    </form>
   );
 }
