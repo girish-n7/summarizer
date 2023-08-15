62a8e518214edaed7527235fb8e8208c55a0e601
Girish_N <ngirish1729@gmail.com>
2023-08-15 22:30:02 +0530

    backend service modifications

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,55 +1,51 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
-import React from "react";
+// import React from "react";
 
 export default function Summary({ summaryObject }) {
-  const summaryList = summaryObject.summary.map((list) => {
-    return <li>{list}</li>;
-  });
+  // const summaryList = summaryObject.summary.map((list) => {
+  //   return <li>{list}</li>;
+  // });
 
-  const authorsList = summaryObject.article_authors.map((author) => {
-    return `${author}, `;
-  });
+  // const authorsList = summaryObject.article_authors.map((author) => {
+  //   return `${author}, `;
+  // });
 
-  const [originalArticle, setOriginalArticle] = React.useState(false);
+  // const [originalArticle, setOriginalArticle] = React.useState(false);
 
-  function handleClick() {
-    setOriginalArticle((prevState) => !prevState);
-  }
+  // function handleClick() {
+  //   setOriginalArticle((prevState) => !prevState);
+  // }
 
-  return summaryObject.key === 1 ? (
+  return (
     <div className="summary--container">
-      <p>Invalid URL</p>
-    </div>
-  ) : (
-    <div className="summary--container">
-      <p>
+      {/* <p>
         <b>Article Title:</b> {summaryObject.article_title}
       </p>
       {summaryObject.article_pub_date && (
         <p>
           <b>Published:</b> {summaryObject.article_pub_date}
         </p>
-      )}
-      {summaryObject.article_authors && (
+      )} */}
+      {/* {summaryObject.article_authors && (
         <p>
           <b>Authors:</b> {authorsList}
         </p>
-      )}
+      )} */}
       <hr className="summary--hr" />
       <p>
         <b>Summary: </b>
       </p>
       <div className="list--container">
-        <ul>{summaryList}</ul>
+        <ul>{summaryObject.summary}</ul>
       </div>
       <hr className="summary--hr" />
-      <div className="summary--button">
+      {/* <div className="summary--button">
         <button onClick={handleClick}>
           {originalArticle ? "Hide" : "Show"} Original Article
         </button>
-      </div>
-      {originalArticle && (
+      </div> */}
+      {/* {originalArticle && (
         <div>
           <div className="article--img">
             <img src={summaryObject.article_image} alt="" />
@@ -59,7 +55,7 @@ export default function Summary({ summaryObject }) {
             {summaryObject.article_text}
           </p>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
